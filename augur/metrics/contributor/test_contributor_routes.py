5b8a8a32cd97b1f11ec5e75ede4d52586f979dc7
zachburk <30427143+zachburk@users.noreply.github.com>
2019-11-14 14:44:39 -0600

    Update test_contributor_routes.py

--- augur/metrics/contributor/test_contributor_routes.py
+++ augur/metrics/contributor/test_contributor_routes.py
@@ -71,4 +71,4 @@ def test_messages_by_contributor_by_repo(metrics):
     response = requests.get('http://localhost:5000/api/unstable/repo-groups/21/repos/21222/messages-by-contributor')
     data = response.json()
     assert response.status_code == 200
-    assert len(data >= 1)
+    assert len(data) >= 1
