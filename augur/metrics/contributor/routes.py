0800b950d183325304c37cd77aa34842b8a33be7
zachburk <30427143+zachburk@users.noreply.github.com>
2019-11-19 12:53:31 -0600

    Update routes.py

--- augur/metrics/contributor/routes.py
+++ augur/metrics/contributor/routes.py
@@ -341,6 +341,22 @@ def create_contributor_routes(server):
 
     server.addRepoMetric(metrics.contributors_code_development, 'contributors-code-development')
 
-    server.addRepoGroupMetric(metrics.messages_by_contributor, 'messages-by-contributor')
-
     server.addRepoMetric(metrics.messages_by_contributor, 'messages-by-contributor')
+    """
+    @api {get} /repo-groups//:repo_group_id/repos/:repo_id/messages-by-contributor Messages By Contributor (Repo)
+    @apiName messages-by-contributor
+    @apiGroup Contributors
+    @apiDescription Returns the number of messages made by each contributor
+    @apiParam {string} repo_group_id Repository Group ID
+    @apiParam {string} repo_id Repository ID
+    @apiSuccessExample
+                [
+                    {
+                        "cntrb_id": 
+                        "messages": 
+                    }
+                ]
+    
+    """
+
+    server.addRepoGroupMetric(metrics.messages_by_contributor, 'messages-by-contributor')
