d988ca1836534ac4973fc979a7db96425a4b269a
Natsukie <aproditeloveu@gmail.com>
2019-11-21 07:22:30 -0600

    new version

---
+++ Sprint-3/FinalSprintProject/src/app/repogroup/repogroup.component.ts
@@ -0,0 +1,20 @@
+import { Component,OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-repogroup',
+  templateUrl: './repogroup.component.html',
+  styleUrls: ['./repogroup.component.css']
+})
+export class RepogroupComponent implements OnInit {
+
+    dataTable: any;
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+
+}
